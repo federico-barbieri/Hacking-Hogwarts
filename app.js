8f886a20701642efb63a751787596b4960d484a9
Federico Barbieri <federicobarbieri54@gmail.com>
2023-02-18 15:39:30 +0100

    make function for levitation

---
+++ app.js
@@ -0,0 +1,9 @@
+const schoolLogo = document.querySelector('.school-logo');
+
+// make something levitate
+
+function makeItLevitate(element){
+    element.classList.add("make-it-levitate");
+}
+
+makeItLevitate(schoolLogo);
